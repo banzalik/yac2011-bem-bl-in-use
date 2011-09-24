1179dbe65068d0ff794032577e592507cfd3bb95
Alexander Boythenko <banzalik@gmail.com>
2011-09-24 11:40:40 +0300

    изменил вывод, добавил сообщение об окончании ребилда

--- bem.rb
+++ bem.rb
@@ -7,8 +7,9 @@ require 'fssm'
 
 FSSM.monitor('pages/', '**/*.bemjson.js') do
 #  update { |b, r| puts "Update in #{b} to #{r}" }
-	update { |b, r| puts "Update in #{b} to #{r}"
-		puts `make` 
+	update { |b, r| puts "Изменения в файле #{b}/#{r}"
+		puts `make`
+		puts "Ребилд завершен" 
 	}
   delete { |b, r| puts "Delete in #{b} to #{r}" }
   create { |b, r| puts "Create in #{b} to #{r}" }
